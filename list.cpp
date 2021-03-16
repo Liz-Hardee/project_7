830fd54f8d82903b878c82a98f225eb1df6cb777
ivyswine <80180839+ivyswine@users.noreply.github.com>
2021-03-16 10:26:30 -0700

    Update list.cpp
    is_empty() and insert()

--- list.cpp
+++ list.cpp
@@ -58,3 +58,36 @@ std::ostream & operator << (std::ostream & out, const list & out_list)
 {
     return out_list.print_list(out);
 }
+
+//insert alphabetically
+bool list::insert(Node * newcomer)
+{
+        Node * curr = head;
+
+        Node * temp = new Node;
+
+        while (*newcomer->data < *curr->data)
+                curr=curr->next; //skip to correct position
+
+        *newcomer->prev = *curr;         //set newcomer's previous pointer
+        newcomer->next = curr->next;    //set newcomer's next pointer to next node
+        *temp = *curr->next;             //put the next node in a temp variable so as not to lose it
+        *curr->next = *newcomer;         //put newcomer in front of current node
+        *curr = *temp;                    //put temp node in current
+        newcomer->next = curr;          //set newcomer's next pointer
+        temp->prev = nullptr;
+        temp->next = nullptr;
+        temp->data->clear();             //delete temp
+
+        return 1;
+}
+
+bool list::is_empty() const  //returns whether list is empty
+{
+        if     (head->data == nullptr 
+             && head->next == nullptr)
+                   return 1;
+        else
+                   return 0;
+}
+
